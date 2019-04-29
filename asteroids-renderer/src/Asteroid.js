04667a9b3d775496644798140aee0751ca29c340
devstopfix <devstopfix@gmail.com>
2019-04-29 10:14:30 +0100

    Reduce asteroid rotation rate by x4

--- asteroids-renderer/src/Asteroid.js
+++ asteroids-renderer/src/Asteroid.js
@@ -3,7 +3,7 @@ import * as shapes from './shapes'
 import * as utils from './utils'
 
 export default class Asteroid {
-    constructor({id, x, y, r},  canvas) {
+    constructor({ id, x, y, r }, canvas) {
 
         this.roid = new fabric.Polygon(shapes.asteroid(r), {
             left: x,
@@ -17,25 +17,24 @@ export default class Asteroid {
 
         this.id = id
         this.canvas = canvas
-
         this.canvas.add(this.roid)
     }
 
-    update({x, y}, duration) {
+    update({ x, y }, duration) {
         let animation = {
             left: x,
             top: y,
-            angle: '+=' + (duration / 5)
+            angle: '+=' + (duration / 20)
         }
 
         //let textAnimation = { left: this.roid.left - 15, top: this.roid.top - 25 }
         let animationSettings = { duration: duration, easing: utils.easing.linear }
 
-        if(Math.abs(this.roid.left - x) > 100) {
+        if (Math.abs(this.roid.left - x) > 100) {
             this.roid.set('left', x)
             //this.text.set('left', x)
         }
-        if(Math.abs(this.top - y) > 100) {
+        if (Math.abs(this.top - y) > 100) {
             this.roid.set('top', y)
             //this.text.set('top', y)
         }
