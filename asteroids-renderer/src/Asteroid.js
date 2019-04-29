252fb5aeec9602f84bc0e715153d158699ac87a8
devstopfix <devstopfix@gmail.com>
2019-04-29 09:54:19 +0100

    Solid asteroids
    Gives them mass

--- asteroids-renderer/src/Asteroid.js
+++ asteroids-renderer/src/Asteroid.js
@@ -4,42 +4,33 @@ import * as utils from './utils'
 
 export default class Asteroid {
     constructor({id, x, y, r},  canvas) {
-        
+
         this.roid = new fabric.Polygon(shapes.asteroid(r), {
             left: x,
             top: y,
-            fill: 'transparent',
+            fill: 'black',
             stroke: 'white',
             selectable: false,
             originX: 'center',
             originY: 'center'
         })
-        
-        // this.text = new fabric.Text(id + '', {
-        //     fontFamily: 'Courier New',
-        //     left: x,
-        //     top: y,
-        //     fontSize: 16,
-        //     fill: 'white'
-        // })
-        
+
         this.id = id
         this.canvas = canvas
-        
+
         this.canvas.add(this.roid)
-        //this.canvas.add(this.text)
     }
-    
+
     update({x, y}, duration) {
         let animation = {
             left: x,
             top: y,
             angle: '+=' + (duration / 5)
         }
-        
+
         //let textAnimation = { left: this.roid.left - 15, top: this.roid.top - 25 }
         let animationSettings = { duration: duration, easing: utils.easing.linear }
-        
+
         if(Math.abs(this.roid.left - x) > 100) {
             this.roid.set('left', x)
             //this.text.set('left', x)
@@ -49,9 +40,8 @@ export default class Asteroid {
             //this.text.set('top', y)
         }
         this.roid.animate(animation, animationSettings)
-        //this.text.animate(textAnimation, animationSettings)
     }
-    
+
     remove() {
         this.canvas.remove(this.roid)
         //this.canvas.remove(this.text)
