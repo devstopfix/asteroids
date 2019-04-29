2c84d604ec31441dd462890a892adc5b0f5125e6
devstopfix <devstopfix@gmail.com>
2019-04-29 10:27:00 +0100

    Solid bullets

--- asteroids-renderer/src/Bullet.js
+++ asteroids-renderer/src/Bullet.js
@@ -6,32 +6,32 @@ import * as utils from './utils'
 export default class Bullet {
     constructor({id, x, y}, canvas) {
         var bullet = new fabric.Circle({
-            radius: 1,
+            radius: 1.2,
             left: x,
             top: y,
-            fill: 'transparent',
-            stroke: 'white',
+            fill: '#7DF9FF',
+            stroke: '#FFFFFF',
             selectable: false,
             originX: 'left',
             originY: 'top',
-            opacity: 0.5
+            opacity: 0.9
         })
-        
+
         canvas.add(bullet)
         this.canvas = canvas
         this.id = id
         this.bullet = bullet
     }
-    
+
     update({x, y}, duration = 50) {
         let animation = {
             left: x,
             top: y,
             opacity: 0.1
         }
-        
+
         let animationSettings = { duration: duration, easing: utils.easing.linear }
-        
+
         if(Math.abs(this.bullet.left - x) > 100) {
             this.bullet.set('left', x)
         }
@@ -40,7 +40,7 @@ export default class Bullet {
         }
         this.bullet.animate(animation, animationSettings)
     }
-    
+
     remove() {
         let self = this;
         setTimeout(function() {
