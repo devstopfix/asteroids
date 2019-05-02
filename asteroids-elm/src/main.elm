16fc358a970d0d0679cb354a8b6b1a000d949f5f
devstopfix <devstopfix@gmail.com>
2019-05-02 12:27:30 +0100

    Canvas

--- asteroids-elm/src/main.elm
+++ asteroids-elm/src/main.elm
@@ -1,4 +1,27 @@
-module HelloWorld exposing (..)
-import Html exposing (text)
-main =
-  text "Hello, Player!"
+module Main exposing (main)
+
+import Canvas exposing (..)
+import Color
+import Html exposing (Html, text)
+import Html.Attributes exposing (style)
+
+
+view : Html msg
+view =
+    let
+        width = 400
+        height = 255
+    in
+        Canvas.toHtml (width, height)
+            [ style "border" "1px solid black" ]
+            [ shapes [ fill Color.black ] [ rect (0, 0) width height ]
+            , renderSquare
+            ]
+
+renderSquare =
+  shapes [ fill (Color.rgba 0 0 0 1) ]
+      [ rect (0, 0) 100 50 ]
+
+main = view
+
+  -- text "Hello, Player!"
