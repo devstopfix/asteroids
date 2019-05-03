8bc7fd22145a964c8e6b98b037fd012d57ff5671
devstopfix <devstopfix@gmail.com>
2019-05-03 12:50:56 +0100

    Refactor to function composition

--- asteroids-elm/src/Points.elm
+++ asteroids-elm/src/Points.elm
@@ -1,33 +1,32 @@
-module Points exposing (convertPoints, closePolygon, readPoints)
+module Points exposing (closePolygon, convertPoints, readPoints)
 
 import Canvas exposing (Point)
 import Point2d exposing (Point2d, coordinates, fromCoordinates)
 
 
-readPoints: List (Float, Float) -> List Point2d
-readPoints ps =
-    List.map (\( x, y ) -> fromCoordinates ( x, y )) ps
+readPoints : List ( Float, Float ) -> List Point2d
+readPoints =
+    List.map fromCoordinates
 
--- List.map fromCoordinates ps
+
 
 -- Convert from Geometry points to Canvas points
+
+
 convertPoints : List Point2d -> List Point
-convertPoints ps =
-    List.map p2p ps
+convertPoints =
+    List.map coordinates
+
+
 
 -- Duplicate the first point as the last point to close the polygon
+
+
 closePolygon : List Point2d -> List Point2d
 closePolygon list =
     case list of
         [] ->
             []
-        p :: ps ->
-            List.append (p :: ps) [p]
 
-p2p : Point2d -> Point
-p2p p =
-    let
-        ( x, y ) =
-            coordinates p
-    in
-    ( x, y )
+        p :: ps ->
+            List.append (p :: ps) [ p ]
