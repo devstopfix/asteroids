b611e3c69a1992a3e6ead0149341edc1844b6e00
devstopfix <devstopfix@gmail.com>
2019-05-06 14:04:49 +0100

    chore: embed resource URL function

--- asteroids-elm/make-prod.sh
+++ asteroids-elm/make-prod.sh
@@ -2,6 +2,7 @@
 
 input=src/main.elm
 canvas=public/elm-canvas.2.2.js
+resources=public/resources.js
 
 target1=public/elixoids.dev.js
 target2=public/elixoids.opt.js
@@ -13,8 +14,6 @@ target6=public/elixoids_canvas.opt.simple.js
 target=public/elixoids.js
 prod_target=../../elixoids/priv/html/elixoids.js
 
-rm $target
-
 time elm make $input            --output $target1
 time elm make $input --optimize --output $target2
 
@@ -23,8 +22,8 @@ time closure-compiler --js $target2 --compilation_level SIMPLE_OPTIMIZATIONS --j
 
 # PROD
 
-time closure-compiler --js $target1 $canvas --compilation_level SIMPLE_OPTIMIZATIONS --language_out ECMASCRIPT_2015 --js_output_file $target5
-if closure-compiler --js $target2 $canvas --compilation_level SIMPLE_OPTIMIZATIONS --language_out ECMASCRIPT_2015 --js_output_file $target6; then
+time closure-compiler --js $target1 $canvas $resources --compilation_level SIMPLE_OPTIMIZATIONS --language_out ECMASCRIPT_2015 --js_output_file $target5
+if closure-compiler --js $target2 $canvas $resources --compilation_level SIMPLE_OPTIMIZATIONS --language_out ECMASCRIPT_2015 --js_output_file $target6; then
     rm $target
     cp $target6 $target
 
